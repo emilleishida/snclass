520bbf9d779a71baa295462e20db5d154ba5ac14
emilleishida <emilleishida@gmail.com>
2015-05-25 22:56:05 +0100

    fix example

--- snclass/bin/fit_plot_lc.py
+++ snclass/bin/fit_plot_lc.py
@@ -22,9 +22,11 @@ def main(args):
     #add extra keys
     lc_data.update(user_input)
 
+    print len(lc_data['g'])
+
     if bool(int(args.calculate)):
         #fit lc
-        lc_data = fit_LC(lc_data)
+        lc_data = fit_LC(lc_data, samples=True)
     else:
         op1 = open(lc_data['samples_dir'][0] + lc_data['file_root'][0] + lc_data['SNID:'][0] + '_samples.dat', 'r')
         lin1 = op1.readlines()
@@ -32,47 +34,45 @@ def main(args):
 
         d1 = [elem.split() for elem in lin1]
 
-        lc_data['xarr_indx'] = {}
+        lc_data['GP_std'] = {}
         for fil in lc_data['filters']:
             lc_data['xarr'][fil] = []
-            lc_data['xarr_indx'][fil] = []
-            lc_data['realizations'][fil] = []
-            for item in d1[0]:
-                if fil == item[0]:
-                    lc_data['xarr'][fil].append(float(item[1:]))
-                    lc_data['xarr_indx'][fil].append(d1[0].index(item))
-
-        for line in d1[1:]:
-            for fil in lc_data['filters']:
-                lc_data['realizations'][fil].append([float(line[i]) for i in lc_data['xarr_indx'][fil]])        
-
-        op2 = open(lc_data['samples_dir'][0] + lc_data['file_root'][0] + lc_data['SNID:'][0] + '_samples.dat', 'r')
+            lc_data['realizations'][fil] = [[float(d1[kk][jj]) for kk in xrange(len(d1)) if d1[kk][0]==fil] 
+                                            for jj in xrange(2, int(lc_data['n_samples'][0]) + 2)]
+ 
+            for i1 in xrange(len(d1)):
+                if d1[i1][0] == fil:
+                    lc_data['xarr'][fil].append(float(d1[i1][1]))
+                
+        op2 = open(lc_data['samples_dir'][0] + lc_data['file_root'][0] + lc_data['SNID:'][0] + '_mean.dat', 'r')
         lin2 = op2.readlines()
         op2.close()
 
         d2 = [elem.split() for elem in lin2]
 
         for fil in lc_data['filters']:
-            lc_data['GP_fit'][fil] = [float(d2[1][j]) for j in lc_data['xarr_indx'][fil]]
+            lc_data['GP_fit'][fil] = [float(d2[j][2]) for j in xrange(1,len(d2)) if d2[j][0] == fil]
+            lc_data['GP_std'][fil] = [float(d2[j][3]) for j in xrange(1,len(d2)) if d2[j][0] == fil]
 
     #initiate figure
-    plt.figure()
+    f = plt.figure()
     
     for fil in user_input['filters']:
 
         # Plot the samples in data space.
-        ax = plt.subplot(len(user_input['filters']), 1, user_input['filters'].index(fil) + 1)
+        ax = plt.subplot(2, len(lc_data['filters'])/2 + 
+                        len(lc_data['filters'])%2, lc_data['filters'].index(fil) + 1)
         for s in lc_data['realizations'][fil]:
-            plt.plot(lc_data['xarr'][fil], s, color="#4682b4", alpha=0.3)
-        plt.errorbar(lc_data[fil][:,0], lc_data[fil][:,1], yerr=lc_data[fil][:,2], fmt=".k", capsize=0, label=fil)
-        plt.plot(lc_data['xarr'][fil], lc_data['GP_fit'][fil], 'r:', linewidth=2)
+            plt.plot(lc_data['xarr'][fil], s, color="gray", alpha=0.3)
+        plt.errorbar(lc_data[fil][:,0], lc_data[fil][:,1], yerr=lc_data[fil][:,2], fmt="o", color='blue', label=fil)
+        plt.plot(lc_data['xarr'][fil], lc_data['GP_fit'][fil], color='red', linewidth=2)
         plt.ylabel("FLUXCAL")
         plt.xlabel("MJD")
         plt.legend()
         plt.xlim(min(lc_data[fil][:,0]) - 1.0, max(lc_data[fil][:,0]) + 1.0)
 
-    plt.suptitle("Results with Gaussian process noise model")
-    plt.savefig("gp-results.png", dpi=350)
+    f.tight_layout()
+    plt.savefig("gp-SN" + lc_data['SNID:'][0] + ".png", dpi=350)
     plt.close()
 
 
